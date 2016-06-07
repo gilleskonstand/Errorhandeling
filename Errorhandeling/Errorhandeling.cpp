f0761cded9fcf3783f4e78918bd85fe4829531f4
gillesk <gillesk@GILLESKW7-LT>
2016-06-07 22:53:38 +1200

    adding head files

--- Errorhandeling/Errorhandeling.cpp
+++ Errorhandeling/Errorhandeling.cpp
@@ -4,9 +4,26 @@
 #include "stdafx.h"
 #include "std_lib_facilities.h"
 
-int main()
-{
-	std::cout << " Hello world " << endl;
-    return 0;
-}
+int area(int length, int width)  // calculate area of a rectangle
+	{
+		return length*width;
+	}
+	int framed_area(int x, int y) // calculate area within frame
+	{
+		return area(x-2, y-2);
+	}
+	int main()
+	{
+		int x = -1;
+		int y = 2;
+		int z = 4;
+		// . . .
+		int area1 = area(x, y);
+		int area2 = framed_area(1, z);
+		int area3 = framed_area(y, z);
+		double ratio = double(area1) / area3; // convert to double to get
+                                              // floating-point division
+		cout << ratio << " |Ratio" << endl;
+		return 0;
+	}
 
